dd37a7c66502a987fbbc972faebb7731f3a4aa50
aleenamalik11 <aleenamalik339@gmail.com>
2025-04-12 11:13:30 +0500

    spring web

--- src/main/java/com/chatbot/api/controller/ChatbotController.java
+++ src/main/java/com/chatbot/api/controller/ChatbotController.java
@@ -1,6 +1,13 @@
 package com.chatbot.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ChatbotController {
 
-	
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from ChatBotApi!";
+	}
 }
