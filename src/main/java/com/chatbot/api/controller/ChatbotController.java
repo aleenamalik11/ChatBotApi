82a981b8d08adab7f1979da00f4787d8347123f4
aleenamalik11 <aleenamalik339@gmail.com>
2025-04-13 19:18:04 +0500

    add swagger

--- src/main/java/com/chatbot/api/controller/ChatbotController.java
+++ src/main/java/com/chatbot/api/controller/ChatbotController.java
@@ -3,10 +3,13 @@ package com.chatbot.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class ChatbotController {
 
 	@GetMapping("/")
+    @Operation(summary = "Welcome Message")
 	public String index() {
 		return "Greetings from ChatBotApi!";
 	}
